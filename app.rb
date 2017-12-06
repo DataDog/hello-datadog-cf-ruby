9b7027d8592d4fbc3d5f535ea59755b9c188d79f
Greg Meyer <greg.meyer@datadoghq.com>
2017-12-06 13:53:54 -0500

    configure with service name

--- app.rb
+++ app.rb
@@ -5,9 +5,12 @@ require 'ddtrace/contrib/sinatra/tracer'
 
 statsd = Datadog::Statsd.new('localhost', 8125)
 
+Datadog.configure do |c|
+  c.use :sinatra, service_name: 'hello-datadog-cf-ruby'
+end
+
 get "/" do
   statsd.increment('page.views', tags: ['index'])
 
   render :html, :index
 end
-
