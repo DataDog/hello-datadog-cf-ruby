f2afd73e01d79a90d20983ed00eb40c08b5db047
Alexandre Jacquemot <alexandre.jacquemot@datadoghq.com>
2018-07-25 11:39:05 +0200

    [Logs]: Added logs to the example app. (#3)

--- app.rb
+++ app.rb
@@ -1,27 +1,36 @@
+require 'logger'
 require "sinatra"
 require 'datadog/statsd'
 require 'ddtrace'
 require 'ddtrace/contrib/sinatra/tracer'
 
+logger = Logger.new(STDOUT)
+logger = Logger.new(STDERR)
+
+logger.info("Starting Stastd server")
 statsd = Datadog::Statsd.new('localhost', 8125)
+
 
 Datadog.configure do |c|
   c.use :sinatra, service_name: 'hello-datadog-cf-ruby'
 end
 
 get "/" do
+  logger.info("Index page hit")
   statsd.increment('page.views', tags: ['page:index'])
 
   render :html, :index
 end
 
 get "/new-page" do
+  logger.info("New page hit")
   statsd.increment('page.views', tags: ['page:new-page'])
 
   render :html, :index
 end
 
 get "/error" do
+  logger.error("Error page hit")
   statsd.increment('page.views', tags: ['page:error-page'])
 
   raise "error"
